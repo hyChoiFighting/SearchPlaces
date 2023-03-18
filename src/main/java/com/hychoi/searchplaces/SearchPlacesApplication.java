49c63cf45af5fa1eba23471573d0898c88adc6ce
choehan-yong <choi.hy6537@gmail.com>
2023-03-18 22:56:57 +0900

    Initial commit

---
+++ src/main/java/com/hychoi/searchplaces/SearchPlacesApplication.java
@@ -0,0 +1,13 @@
+package com.hychoi.searchplaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchPlacesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchPlacesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hychoi/searchplaces/SearchPlacesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hychoi.searchplaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchPlacesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
